7a6e8976c865854ec2f3729cb13833f87501a9ac
L_aZY <18846171630@163.com>
2019-01-03 21:42:10 +0800

    测试代码

---
+++ algorithm/src/int_que11/Test.java
@@ -0,0 +1,13 @@
+package int_que11;
+
+/**
+ * @author LaZY（李志一）
+ * @data 2019/1/3 - 21:34
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(Integer_power.power(2, 4));
+        System.out.println(Integer_power.power(2, -4));
+        System.out.println(Integer_power.power(2, 0));
+    }
+}
